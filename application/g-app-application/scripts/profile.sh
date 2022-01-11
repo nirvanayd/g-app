bf8589c87ce79d07b41e979f57f400bba4e4604f
jeon-yundong <yd.jeon@adictives.com>
2022-01-11 23:19:20 +0900

    blue-grenn 배포 환경 설정

--- application/g-app-application/scripts/profile.sh
+++ application/g-app-application/scripts/profile.sh
@@ -40,7 +40,7 @@ function find_idle_port()
 
 function find_current_port()
 {
-    # curl 결과로 연결할 서비스 결정
+        # curl 결과로 연결할 서비스 결정
         RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/prod-profile)
 
         if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
@@ -50,12 +50,13 @@ function find_current_port()
             CURRENT_PROFILE=$(curl -s http://localhost/prod-profile)
         fi
 
+        echo "${CURRENT_PROFILE}"
         # IDLE_PROFILE : nginx와 연결되지 않은 profile
-        if [ ${CURRENT_PROFILE} == "prod-blue" ]
-        then
-            echo "8081"
-        else
-            echo "8082"
-        fi
+#        if [ "${CURRENT_PROFILE}" == "prod-blue" ]
+#        then
+#            echo "8081"
+#        else
+#            echo "8082"
+#        fi
 }
 
