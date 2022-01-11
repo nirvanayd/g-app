bcfcfcfd5c7b7ae4df6b073a85cc6c792445a214
jeon-yundong <yd.jeon@adictives.com>
2022-01-12 00:10:48 +0900

    blue-grenn 배포 환경 설정

--- application/g-app-application/src/main/java/com/nelly/application/controller/TestController.java
+++ application/g-app-application/src/main/java/com/nelly/application/controller/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("hello..world!!");
+        return ResponseEntity.ok("Hello World!");
     }
 
     @GetMapping("/env")
