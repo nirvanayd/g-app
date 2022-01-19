58c743e0aedddfda788eb58fc0b5e74ba1b3046d
jeon-yundong <yd.jeon@adictives.com>
2022-01-20 01:12:46 +0900

    admin web 설정 추가

---
+++ application/g-admin-applicaion/src/main/java/com/nelly/application/enums/Profiles.java
@@ -0,0 +1,50 @@
+package com.nelly.application.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Getter
+public enum Profiles {
+    LOCAL("local"),
+    LOCAL_BLUE("local-blue"),
+    LOCAL_GREEN("local-green"),
+    PROD_BLUE("prod-blue"),
+    PROD_GREEN("prod-green");
+
+    private final String name;
+
+    public static List<String> getList() {
+        return Arrays.stream(Profiles.values()).map(Profiles::getName).collect(Collectors.toList());
+    }
+
+    public static List<String> getLocalList() {
+        return Arrays.stream(Profiles.values()).map(Profiles::getName).
+                filter(pName -> pName.startsWith("local")).collect(Collectors.toList());
+    }
+
+    public static List<String> getProdList() {
+        return Arrays.stream(Profiles.values()).map(Profiles::getName).
+                filter(pName -> pName.startsWith("prod")).collect(Collectors.toList());
+    }
+
+    public static String hasProfile(String env, String profile) {
+        List<String> compare = null;
+        if (env.equals("local")) {
+            compare = getLocalList();
+        } else if (env.equals("prod")) {
+            compare = getProdList();
+        }
+
+        if (compare == null) return "not supported..";
+
+        return compare.stream()
+                .filter(c -> c.equals(profile))
+                .findAny()
+                .orElse("not supported..");
+    }
+}
