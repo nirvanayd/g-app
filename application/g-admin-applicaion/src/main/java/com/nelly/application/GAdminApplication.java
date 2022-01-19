8bb7206bc90145234b0d24be2420f94017af9711
jeon-yundong <yd.jeon@adictives.com>
2022-01-19 22:58:01 +0900

    admin controller 작성

---
+++ application/g-admin-applicaion/src/main/java/com/nelly/application/GAdminApplication.java
@@ -0,0 +1,13 @@
+package com.nelly.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class GAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GAdminApplication.class, args);
+    }
+}
