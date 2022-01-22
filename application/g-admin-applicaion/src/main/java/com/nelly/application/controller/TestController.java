cc02a880b3e36aa89c6549a88c6159aa8630e75d
jeon-yundong <yd.jeon@adictives.com>
2022-01-22 09:59:28 +0900

    admin 환경변수 추가

--- application/g-admin-applicaion/src/main/java/com/nelly/application/controller/TestController.java
+++ application/g-admin-applicaion/src/main/java/com/nelly/application/controller/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("Hello World!!");
+        return ResponseEntity.ok("master-api");
     }
 
     @GetMapping("/env")
