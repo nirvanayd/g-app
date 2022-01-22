b9d8016e4920dea5ee1488275ec755ecb6f4cb54
jeon-yundong <yd.jeon@adictives.com>
2022-01-22 11:46:37 +0900

    git actions for api test

--- application/g-app-application/src/main/java/com/nelly/application/controller/TestController.java
+++ application/g-app-application/src/main/java/com/nelly/application/controller/TestController.java
@@ -12,7 +12,7 @@ public class TestController {
 
     @GetMapping("/hello")
     public ResponseEntity<String> hello() {
-        return ResponseEntity.ok("api test!");
+        return ResponseEntity.ok("api branch test");
     }
 
     @GetMapping("/env")
