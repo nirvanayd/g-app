e57ca52bd962fedbd2a95a52060be8e859290f0b
jeon-yundong <yd.jeon@adictives.com>
2022-10-28 20:40:05 +0900

    redirection 오류 수정 중

--- client/g-app-core-web/src/main/java/com/nelly/application/handler/CustomAuthenticationEntryPoint.java
+++ client/g-app-core-web/src/main/java/com/nelly/application/handler/CustomAuthenticationEntryPoint.java
@@ -14,6 +14,6 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.setStatus(411);
+        response.setStatus(410);
     }
 }
