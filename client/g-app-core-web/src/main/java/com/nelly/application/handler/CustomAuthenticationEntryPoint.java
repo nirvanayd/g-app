b157d8a7e4d3a20541061513d9aa0fb3cb57a610
jeon-yundong <yd.jeon@adictives.com>
2022-10-28 20:24:59 +0900

    스크랩 기능 수정

--- client/g-app-core-web/src/main/java/com/nelly/application/handler/CustomAuthenticationEntryPoint.java
+++ client/g-app-core-web/src/main/java/com/nelly/application/handler/CustomAuthenticationEntryPoint.java
@@ -14,6 +14,6 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
-        response.sendRedirect("/exception/entryPoint");
+        response.setStatus(411);
     }
 }
